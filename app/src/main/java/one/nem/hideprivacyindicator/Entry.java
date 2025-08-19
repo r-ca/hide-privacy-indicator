6858705e2eef695c58afd68588bb5b1389df06e7
rca <nem@nem.x0.to>
2025-08-19 16:12:20 +0900

    impl logic

--- app/src/main/java/one/nem/hideprivacyindicator/Entry.java
+++ app/src/main/java/one/nem/hideprivacyindicator/Entry.java
@@ -1,4 +1,13 @@
 package one.nem.hideprivacyindicator;
 
-public class Entry {
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+
+public class Entry implements IXposedHookInitPackageResources {
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam initParam) {
+        if (!initParam.packageName.equals("com.android.systemui")) return;
+
+        initParam.res.setReplacement("com.android.systemui", "bool", "config_enablePrivacyDot",false);
+    }
 }
